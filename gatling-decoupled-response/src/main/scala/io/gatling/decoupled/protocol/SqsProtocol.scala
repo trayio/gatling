10a1a3372a795bd5f11c4819f88aac8457345ee3
Simone Fabiano <simone@tray.io>
2020-04-24 14:35:10 +0100

    [PSP-2033] Expose AWS keys in the SQS protocol (#7)
    * [PSP-2033] Expose AWS keys in the SQS protocol
    
    * [PSP-2033] Return same components for the same protocol

--- gatling-decoupled-response/src/main/scala/io/gatling/decoupled/protocol/SqsProtocol.scala
+++ gatling-decoupled-response/src/main/scala/io/gatling/decoupled/protocol/SqsProtocol.scala
@@ -18,10 +18,12 @@ package io.gatling.decoupled.protocol
 
 import java.net.URL
 
+import com.github.benmanes.caffeine.cache.{ Cache, Caffeine }
 import io.gatling.core.CoreComponents
 import io.gatling.core.config.GatlingConfiguration
 import io.gatling.core.protocol.{ Protocol, ProtocolComponents, ProtocolKey }
 import io.gatling.core.session.Session
+import io.gatling.decoupled.ingestion.SqsReader.AwsKeys
 import io.gatling.decoupled.ingestion.{ SqsMessageProcessor, SqsReader }
 import io.gatling.decoupled.state.{ ActorBasedPendingRequestsState, PendingRequestsState }
 import software.amazon.awssdk.regions.Region
@@ -35,24 +37,33 @@ object SqsProtocol {
     override def defaultProtocolValue(configuration: GatlingConfiguration): SqsProtocol =
       throw new IllegalStateException("Can't provide a default value for SqsProtocol")
 
+    private val components: Cache[SqsProtocol, SqsComponents] = Caffeine.newBuilder().build()
+
     override def newComponents(coreComponents: CoreComponents): SqsProtocol => SqsComponents = { protocol =>
-      val state = ActorBasedPendingRequestsState(
-        coreComponents,
-        protocol.decoupledResponseTimeout,
-        protocol.processingTimeout
+      components.get(
+        protocol,
+        createSqsComponents(coreComponents, _)
       )
-
-      val sqsReader = new SqsReader(
-        new SqsMessageProcessor(state),
-        protocol.awsRegion,
-        protocol.queueUrl.toExternalForm,
-        None
-      )(coreComponents.actorSystem)
-
-      sqsReader.run
-
-      SqsComponents(state, sqsReader)
     }
+  }
+
+  private def createSqsComponents(coreComponents: CoreComponents, protocol: SqsProtocol): SqsComponents = {
+    val state = ActorBasedPendingRequestsState(
+      coreComponents,
+      protocol.decoupledResponseTimeout,
+      protocol.processingTimeout
+    )
+
+    val sqsReader = new SqsReader(
+      new SqsMessageProcessor(state),
+      protocol.awsRegion,
+      protocol.queueUrl.toExternalForm,
+      protocol.awsKeys
+    )(coreComponents.actorSystem)
+
+    sqsReader.run
+
+    SqsComponents(state, sqsReader)
   }
 
   def default(awsRegion: String, queueUrl: String): SqsProtocol = {
@@ -60,7 +71,8 @@ object SqsProtocol {
       new URL(queueUrl),
       Region.of(awsRegion),
       10.minutes,
-      30.seconds
+      30.seconds,
+      None
     )
   }
 
@@ -70,7 +82,8 @@ final case class SqsProtocol(
     queueUrl: URL,
     awsRegion: Region,
     decoupledResponseTimeout: FiniteDuration,
-    processingTimeout: FiniteDuration
+    processingTimeout: FiniteDuration,
+    awsKeys: Option[AwsKeys]
 ) extends Protocol
 
 final case class SqsComponents(pendingRequests: PendingRequestsState, reader: SqsReader) extends ProtocolComponents {
